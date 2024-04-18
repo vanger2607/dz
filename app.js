ec5b8a32d0ee1fd5407bed55a048371f51a60b3c
vanger2607 <vanger2607@gmail.com>
2024-04-18 15:30:33 +0300

    feat body-parse middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require("body-parser")
 // Импортируем роутеры
 const {mainRouter, gamesRouter} = require('./routes');
 
@@ -10,7 +11,7 @@ const app = express();
 
 // Запускаем
 app.use(express.static(path.join(__dirname, 'public'))); 
-app.use(mainRouter, gamesRouter); 
+app.use(bodyParser.json(), mainRouter, gamesRouter); 
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT http://localhost:${PORT}`);
