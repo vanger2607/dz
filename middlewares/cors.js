6f51584767d7885e6881f7c96f47c47676bb15d3
vanger2607 <vanger2607@gmail.com>
2024-04-18 16:03:04 +0300

    refactor: add list of trusting-sites

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,5 +1,16 @@
+const allowedCors = [
+  "https://practicum.yandex.ru",
+  "https://students-projects.ru",
+  "localhost:3000",
+];
+
 function cors(req, res, next) {
-    const { origin } = req.headers;
-    res.header('Access-Control-Allow-Origin', 'localhost:3000');
-    next();
-} 
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    // Если это наш друг
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  next();
+}
