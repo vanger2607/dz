d6b89158e2fd7f18cbe658bc71cb8fe75dc670bd
vanger2607 <vanger2607@gmail.com>
2024-04-18 15:37:21 +0300

    feat create middleware getAllGames

---
+++ middlewares/games.js
@@ -0,0 +1,17 @@
+const { readData, } = require("../utils/data");
+
+const getAllGames = async (req, res, next) => {
+     // Читаем список игр из файла
+     const games = await readData("./data/games.json");
+     if (!games) {
+         res.status(400);
+         res.send({
+         status: "error",
+         message: "Нет игр в базе данных. Добавьте игру.",
+         });
+         return;
+     }
+     req.games = games;
+     next();
+}
+module.exports = getAllGames;
