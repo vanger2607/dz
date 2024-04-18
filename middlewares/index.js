e7535035bc02f660bc1349f900eb076472507735
vanger2607 <vanger2607@gmail.com>
2024-04-18 16:53:12 +0300

    feat: add new imports

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,19 @@
-const getAllGames = require("./games");
+const {
+  getAllGames,
+  checkIsTitleInArray,
+  updateGamesArray,
+  updateGamesFile,
+  findGameById,
+  deleteGame,
+} = require("./games");
 const cors = require("./cors");
 
-module.exports = { getAllGames, cors };
+module.exports = {
+  getAllGames,
+  checkIsTitleInArray,
+  updateGamesArray,
+  updateGamesFile,
+  findGameById,
+  deleteGame,
+  cors,
+};
