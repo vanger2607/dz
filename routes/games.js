03a965acbf9ac091583dd1831c4d60ce29c9d320
vanger2607 <vanger2607@gmail.com>
2024-04-18 15:53:20 +0300

    feat: add middleware getAllGames to games routers

--- routes/games.js
+++ routes/games.js
@@ -1,8 +1,9 @@
 const gamesRouter = require("express").Router(); // Создали роутер
 const {sendAllGames, deleteGame, addGameController} = require("../controllers")
+const { getAllGames } = require("../middlewares")
 
 
-
+gamesRouter.use(getAllGames)
 gamesRouter.get("/games", sendAllGames)
 gamesRouter.delete("/games/:id", deleteGame);
 gamesRouter.post("/games", addGameController) 
