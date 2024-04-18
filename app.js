51485b5cc027cac2c5222896c33cabd256876558
vanger2607 <vanger2607@gmail.com>
2024-04-18 16:12:00 +0300

    feat: add middleware cors to app

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require("body-parser")
+const express = require("express");
+const path = require("path");
+// Импортируем middleware
+const bodyParser = require("body-parser");
+const { cors } = require("./middlewares");
 // Импортируем роутеры
-const {mainRouter, gamesRouter} = require('./routes');
-
-
+const { mainRouter, gamesRouter } = require("./routes");
 
 const PORT = 3000;
 const app = express();
 
 // Запускаем
-app.use(express.static(path.join(__dirname, 'public'))); 
-app.use(bodyParser.json(), mainRouter, gamesRouter); 
-
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json(), mainRouter, gamesRouter);
+app.use(cors);
 app.listen(PORT, () => {
   console.log(`Server is running at PORT http://localhost:${PORT}`);
-}) 
+});
